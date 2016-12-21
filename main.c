520aec93be9f317d1e0ad3fc028ef1ee7a26e4af
gardners <paul@servalproject.org>
2016-12-21 17:21:15 +0100

    try implementing uboot memory modify commands.

--- main.c
+++ main.c
@@ -4,10 +4,37 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <strings.h>
+#include <unistd.h>
 
 #include "gertrude.h"
 
 int serialfd=-1;
+
+int compare_word(unsigned int offset,unsigned int w,unsigned char *data)
+{
+  int r=0;
+  unsigned char w0=(w>>0)&0xff;
+  unsigned char w1=(w>>8)&0xff;
+  unsigned char w2=(w>>16)&0xff;
+  unsigned char w3=(w>>24)&0xff;
+  if (data[offset+0]!=w0) {
+    printf("0x%08x : Saw %02x instead of %02x\n",offset+0,w0,data[offset+0]);
+    r++;
+  }
+  if (data[offset+1]!=w1) {
+    printf("0x%08x : Saw %02x instead of %02x\n",offset+1,w1,data[offset+1]);
+    r++;
+  }
+  if (data[offset+2]!=w2) {
+    printf("0x%08x : Saw %02x instead of %02x\n",offset+2,w2,data[offset+2]);
+    r++;
+  }
+  if (data[offset+3]!=w3) {
+    printf("0x%08x : Saw %02x instead of %02x\n",offset+3,w3,data[offset+3]);
+    r++;
+  }
+  return r;
+}
 
 int main(int argc,char **argv)
 {
@@ -68,5 +95,97 @@ int main(int argc,char **argv)
   
   printf("Attempting to load '%s' @ 0x%llx (length = 0x%llx, entry point = 0x%llx)\n",
 	 file,loadaddress,imagebytes,entryaddress);
+
+  for(int offset=0;offset<stat.st_size;offset+=1024) {
+
+    // How many bytes in current block
+    int count=1024;
+    if (count>(stat.st_size-offset)) count=stat.st_size-offset;
+
+    int errors=1;
+
+    while(errors) {
+      // Write data
+      char cmd[1024];
+
+      // Read back and verify
+      snprintf(cmd,1024,"md.l %x 256\r\n",offset);
+      write_all(fd,cmd,strlen(cmd));
+      
+      if (!errors) break;
+
+      printf("Writing at 0x%08x\n",offset);      
+      
+      // Start memory write with advance
+      snprintf(cmd,1024,"mw.l %x\r\n",offset);
+      write_all(fd,cmd,strlen(cmd));
+
+      unsigned char buf[1024];
+      int r;
+      unsigned long long expected_offset=0x1000000+offset;
+      unsigned long long last_expected_offset=offset+1024-16;
+      char mdline[1024];
+      int mdlinelen=0;
+      unsigned int addr,w1,w2,w3,w4;
+      while(expected_offset<=last_expected_offset) {
+	r=read_nonblock(fd,buf,1024);
+	buf[r]=0;
+	for(int k=0;k<r;k++)
+	  if (buf[k]=='\n') {
+	    // check line
+	    mdline[mdlinelen]=0;
+	    printf("saw '%s'\n",mdline);
+	    if (sscanf(mdline,"%x: %x %x %x %x",
+		       &addr,&w1,&w2,&w3,&w4)==5) {
+	      printf("Valid line: %08x : %08x %08x %08x %08x\n",
+		     addr,w1,w2,w3,w4);
+	      errors+=compare_word(addr-0x1000000,w1,data);
+	      errors+=compare_word(addr-0x1000000+4,w2,data);
+	      errors+=compare_word(addr-0x1000000+8,w3,data);
+	      errors+=compare_word(addr-0x1000000+12,w4,data);
+	      if (addr==expected_offset) expected_offset+=16;
+	    }
+	    mdlinelen=0;
+	  } else {
+	    if (mdlinelen<1024) mdline[mdlinelen++]=buf[k];
+	  }
+	if (r<1) usleep(10000);
+      }
+
+      
+
+      int question_marks_expected=0;
+      int question_marks_received=0;
+      
+      for(int j=0;j<count;j++)
+	{
+
+	  question_marks_expected++;
+
+	  // Read from serial port until we get a ? mark
+	  unsigned char buf[1024];
+	  int r=read_nonblock(fd,buf,1024);
+	  buf[r]=0;
+	  for(int k=0;k<r;k++) if (buf[k]=='?') question_marks_received++;
+	  while (question_marks_expected>question_marks_received) {
+	    printf("  Waiting for serial port to catch up...\n");
+	    r=read_nonblock(fd,buf,1024);
+	    buf[r]=0;
+	    for(int k=0;k<r;k++) if (buf[k]=='?') question_marks_received++;
+	    if (r<1) usleep(10000);
+	  }
+
+	  // Write bytes
+	  snprintf(cmd,1024,"%02x%02x%02x%02x\n\r",
+		   data[offset+j+0],
+		   data[offset+j+1],
+		   data[offset+j+2],
+		   data[offset+j+3]);
+	  write_all(fd,cmd,strlen(cmd));
+	}
+      
+    }
+  }
+  
   
 }
